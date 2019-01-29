3755b2ab8406511581f2667c13e92d91a8bc2d96
bobax <bobax@localhost.localdomain>
2019-01-29 23:30:15 +0300

    Initial commit

---
+++ Classes/constants.py
@@ -0,0 +1,5 @@
+from Classes.aircrack import aircrack
+class constants:
+    userCommandDict = {
+        '1' : aircrack.a(5),
+    }
---
+++ main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import os
+from Classes.constants import constants
+a = input("Enter command\n")
+def swithUserCommand(string):
+    print("2")
+swithUserCommand(a)
