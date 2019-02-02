75953a7e1e774b675fd5bb7ff2fc8a9fff248593
bobax <romanov.tag@mail.ru>
2019-02-02 22:17:49 +0300

    work with subprocess

--- Classes/aircrack.py
+++ Classes/aircrack.py
@@ -1,6 +1,12 @@
+import subprocess
+import time
 import os
+import signal
 class aircrack:
     @staticmethod
     def monitor_mode():
-        result = os.system("airodump-ng --write test wlan0mon")
+        cmd = 'sudo airodump-ng --write txt wlan0mon'
+        result = subprocess.Popen(cmd,shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        time.sleep(10)
+        result.kill()
 
